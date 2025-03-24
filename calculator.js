af492a98c3ffb888eabe7d3ade965300ab5d0d19
Igiraneza1 <134881953+Igiraneza1@users.noreply.github.com>
2025-03-24 10:25:43 +0200

    favorite commit

---
+++ calculator.js
@@ -0,0 +1,11 @@
+const a = 20;
+const b = 5;
+var sum = a+b;
+var difference = a-b;
+var product = a*b;
+var  quotient = a/b;
+console.log(`Sum is: ${sum}`);
+console.log(`Difference is: ${difference}`);
+console.log(`Product is: ${product}`);
+console.log(`Quotient is: ${quotient}`);
+
---
+++ favorite.js
@@ -0,0 +1,4 @@
+const favoriteFood = "pizza";
+const favoriteColor = "red";
+const hobby = "travelling";
+console.log(`Favorite food is: ${favoriteFood}, Favorite color is: ${favoriteColor}, Hobby is: ${hobby}`);
